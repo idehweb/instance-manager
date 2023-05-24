0279d129ee9f962170f1c796af2c6bac8e234292
Erfan <erfanmahvash12345@gmail.com>
2023-05-24 15:43:26 +0330

    add prefix to instance name

--- src/job/executer.js
+++ src/job/executer.js
@@ -18,7 +18,7 @@ export default class Executer {
     this.job = job;
     this.instance = instance;
     this.log_file = fs.createWriteStream(
-      getPublicPath(`logs/${String(this.job._id)}-${this.instance.name}.log`),
+      getPublicPath(`logs/${String(this.job._id)}-${this.instance_name}.log`),
       { encoding: "utf8" }
     );
   }
@@ -28,12 +28,16 @@ export default class Executer {
     executerNode.execute().then().catch();
   }
 
+  get instance_name() {
+    return `nwi-${this.instance.name}`;
+  }
+
   log(chunk, isEnd = false, isError = false, whenDifferent = false) {
     if (this.last_log == String(chunk) && whenDifferent) return;
     this.last_log = String(chunk);
 
     const chunk_with_id =
-      `#${this.instance.name}-${String(this.job._id).slice(0, 8)}#: ` + chunk;
+      `#${this.instance_name}-${String(this.job._id).slice(0, 8)}#: ` + chunk;
 
     // console log
     console.log(chunk_with_id);
@@ -122,13 +126,13 @@ export default class Executer {
 
   async #create_instance() {
     // create public
-    const createFolders = `mkdir -p /var/${this.instance.name} && mkdir -p /var/${this.instance.name}/shared && mkdir -p /var/${this.instance.name}/public`;
+    const createFolders = `mkdir -p /var/${this.instance_name} && mkdir -p /var/${this.instance_name}/shared && mkdir -p /var/${this.instance_name}/public`;
     this.log(createFolders);
     await this.#exec(createFolders);
 
     // copy static files
     const copyStatics = `cp -r ${getPublicPath("static")}/* /var/${
-      this.instance.name
+      this.instance_name
     }/public`;
     this.log(copyStatics);
     await this.#exec(copyStatics);
@@ -137,27 +141,27 @@ export default class Executer {
     const dockerServiceLs = `docker service ls --format "{{.Name}} {{.Replicas}}"`;
     this.log(dockerServiceLs);
     const listServices = (await this.#exec(dockerServiceLs)).split("\n");
-    const myService = listServices.find((s) => s.includes(this.instance.name));
+    const myService = listServices.find((s) => s.includes(this.instance_name));
 
     // create docker service
     const dockerCreate = `docker service create --hostname ${
-      this.instance.name
+      this.instance_name
     } --name ${
-      this.instance.name
+      this.instance_name
     } -e PUBLIC_PATH=/app/public -e SHARED_PATH=/app/shared -e mongodbConnectionUrl="mongodb://mongomaster:27017,mongoslave1:27017,mongoslave2:27017/?replicaSet=mongoReplica" -e SERVER_PORT=3000 -e BASE_URL="https://${
       this.instance.name
     }.nodeeweb.com" -e SHOP_URL="https://${
       this.instance.name
     }.nodeeweb.com/" --mount type=bind,source=/var/${
-      this.instance.name
+      this.instance_name
     }/shared/,destination=/app/shared/  --mount type=bind,source=/var/${
-      this.instance.name
+      this.instance_name
     }/public/,destination=/app/public/  --mount type=bind,source=/var/${
-      this.instance.name
+      this.instance_name
     }/public/public_media/,destination=/app/public_media/  --mount type=bind,source=/var/${
-      this.instance.name
+      this.instance_name
     }/public/admin/,destination=/app/admin/  --mount type=bind,source=/var/${
-      this.instance.name
+      this.instance_name
     }/public/theme/,destination=/app/theme/ --network nodeeweb_webnet --network nodeeweb_mongonet --replicas ${
       this.instance.replica
     } ${this.instance.cpu === -1 ? "" : `--limit-cpu=${this.instance.cpu}`} ${
@@ -173,7 +177,7 @@ export default class Executer {
     if (myService) {
       if (myService.split(" ")[1].startsWith("0")) {
         // must remove service
-        const dockerRm = `docker service rm ${this.instance.name}`;
+        const dockerRm = `docker service rm ${this.instance_name}`;
         this.log(dockerRm);
         await this.#exec(dockerRm);
 
