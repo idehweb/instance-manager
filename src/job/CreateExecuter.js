c5f52290686e415113fc3a37c5ca340de930afc9
Erfan <erfanmahvash12345@gmail.com>
2023-12-16 15:55:26 +0330

    fix: persmissions

--- src/job/CreateExecuter.js
+++ src/job/CreateExecuter.js
@@ -65,7 +65,7 @@ export default class CreateExecuter extends BaseExecuter {
 
   async static_permissions() {
     const publicPath = join(nameToDir(this.instance_name), "public");
-    const cmd = `chgrp -R public ${publicPath} && chmod 774 -R ${publicPath}`;
+    const cmd = `chgrp -R public ${publicPath} && chmod 775 -R ${publicPath}`;
     await this.exec(cmd);
   }
 
